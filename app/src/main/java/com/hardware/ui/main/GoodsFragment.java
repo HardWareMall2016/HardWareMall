7ffe8532fd4710b739e385151143e149e2e87088
hover0 <hover_android@163.com>
2016-04-12 21:04:23 +0800

    GoodsFragment 改为继承framework

--- app/src/main/java/com/hardware/ui/main/GoodsFragment.java
+++ app/src/main/java/com/hardware/ui/main/GoodsFragment.java
@@ -6,38 +6,28 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.hardware.R;
 import com.hardware.adapter.GoodsCategoryAdapter;
 import com.hardware.adapter.GridSpacingItemDecoration;
-
-import butterknife.Bind;
-import butterknife.ButterKnife;
+import com.zhan.framework.support.inject.ViewInject;
+import com.zhan.framework.ui.fragment.ABaseFragment;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class GoodsFragment extends Fragment {
-    @Bind(R.id.recyclerView)
+public class GoodsFragment extends ABaseFragment {
+    @ViewInject(id = R.id.recyclerView)
     RecyclerView recylcer;
 
-    public GoodsFragment() {
-    }
-
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_goods, container, false);
-        ButterKnife.bind(this, view);
-        return view;
+    protected int inflateContentView() {
+        return R.layout.fragment_goods;
     }
 
     @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
+    protected void layoutInit(LayoutInflater inflater, Bundle savedInstanceSate) {
+        super.layoutInit(inflater, savedInstanceSate);
         int spanCount = 3;
         int spacing = 50;
         boolean includeEdge = true;
