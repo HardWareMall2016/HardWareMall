da98b686b7a0899171de62f411645f2b0ef1f9d4
keke <Value.Pu@zhan.com>
2016-04-15 15:41:35 +0800

    地址修改

--- app/src/main/java/com/hardware/base/Constants.java
+++ app/src/main/java/com/hardware/base/Constants.java
@@ -4,7 +4,7 @@ package com.hardware.base;
  * Created by WuYue on 2016/4/11.
  */
 public class Constants {
-    public final static String REGION_NAME="上海市,上海市,静安区";
+    public final static String REGION_NAME="上海市,静安区";
 
     // 服务地址
     public static final String CHECK_UPDATE_URL = "check_update_url";
